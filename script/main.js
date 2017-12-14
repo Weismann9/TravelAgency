9155c42834cf0e5dcaabcad553919c7970d5e628
Dmytro Karpovych <ZAYEC77@gmail.com>
2017-12-14 03:02:54 +0200

    small fixes

--- script/main.js
+++ script/main.js
@@ -1,84 +1,78 @@
 //tabs
-$(function() {
-	$("#tabs").tabs();
+$(function () {
+    $("#tabs").tabs();
 });
 
-$(function() {
-	$(".datepicker").each(function() {
-		$(this).datepicker({
-
-		});
-	});
-	$(".spinner").each(function() {
-		$(this).spinner({
-			classes: {
-				"ui-spinner": "form-control"
-			}
-		});
-	});
-    $( ".checkbox-radio" ).checkboxradio({
-      icon: false
+$(function () {
+    $(".datepicker").each(function () {
+        $(this).datepicker({});
+    });
+    $(".spinner").each(function () {
+        $(this).spinner({
+            classes: {
+                "ui-spinner": "form-control"
+            }
+        });
+    });
+    $(".checkbox-radio").checkboxradio({
+        icon: false
     });
 
-    $( ".slider-range-price" ).slider({
-      range: true,
-      min: 0,
-      max: 5000,
-      values: [ 0, 0 ],
-      slide: function( event, ui ) {
-        $( "#price" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
+    $(".slider-range-price").slider({
+        range: true,
+        min: 0,
+        max: 5000,
+        values: [0, 0],
+        slide: function (event, ui) {
+            $("#price").val("$" + ui.values[0] + " - $" + ui.values[1]);
+        }
     });
 
-    $( ".slider-range-rating" ).slider({
-      range: true,
-      min: 0,
-      max: 5,
-      values: [ 0, 0 ],
-      slide: function( event, ui ) {
-        $( "#rating" ).val( "Зірок " + ui.values[ 0 ] + " - " + ui.values[ 1 ] );
-      }
+    $(".slider-range-rating").slider({
+        range: true,
+        min: 0,
+        max: 5,
+        values: [0, 0],
+        slide: function (event, ui) {
+            $("#rating").val("Зірок " + ui.values[0] + " - " + ui.values[1]);
+        }
     });
-	$(".form").validate({
-		rules: {
-			nights : "required",
-		},
-		messages: {
-			dataFrom : "Please enter the date",
-		}
-	});
-	$( ".accordion" ).accordion({
-	      heightStyle: "content"
+    $(".accordion").accordion({
+        heightStyle: "content"
     });
-	});
+});
 
-//autocomplete
-$(function() {
-	var availableTags = [
-		"Київ",
-		"Москва",
-		"Лондон",
-		"BASIC",
-		"C",
-		"C++",
-		"Clojure",
-		"COBOL",
-		"ColdFusion",
-		"Erlang",
-		"Fortran",
-		"Groovy",
-		"Haskell",
-		"Java",
-		"JavaScript",
-		"Lisp",
-		"Perl",
-		"PHP",
-		"Python",
-		"Ruby",
-		"Scala",
-		"Scheme"
-	];
-	$("#tags").autocomplete({
-		source: availableTags
-	});
+
+//form
+$(function () {
+    $('#searchForm').validate({
+        rules: {
+            city: {valueNotEquals: "default"},
+            dataFrom: 'required',
+            nights: {
+                required: true,
+                min: 1
+            }
+        },
+        submitHandler: function (form, e) {
+            $('#result').fadeIn();
+            $("html, body").animate({scrollTop: $(document).height() + 100}, 1000);
+            e.preventDefault(e);
+            return false;
+        }
+    });
+});
+
+$.validator.addMethod("valueNotEquals", function (value, element, arg) {
+    return arg !== $(element).find('option:selected').val();
+}, "Це поле є обов'язковим.");
+
+$.extend(jQuery.validator.messages, {
+    required: "Це поле є обов'язковим.",
+    min: jQuery.validator.format("Будь ласка, введіть значення більше або рівне {0}"),
+});
+
+
+$('.checkout').on('click', function () {
+    $('#modal').modal()
 });
